2cc502ec186167c227d54baf8881fef664e27bc6
Lucas Levi <lucas.lsl12@gmail.com>
2021-01-06 19:32:47 -0300

    Changing index route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('WELCOME!!!') }}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' =>'web'], function(){
 
-Route::get('/', function () {
-    return view('welcome');
+	Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+	Auth::routes();
+	Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
